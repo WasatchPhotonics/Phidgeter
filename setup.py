a02dad82a62533caf3bc2afdd8dcffd1daf8f7f5
Nathan Harrington <nharrington@wasatchphotonics.com>
2015-08-14 20:11:43 -0400

    install requirements in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/nharringtonwasatch/Phidgeter',
     'author_email': 'nharrington@wasatchphotonics.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose', 'testfixtures', 'phidgets'],
     'packages': ['phidgeter'],
     'scripts': [],
     'name': 'phidgeter'
