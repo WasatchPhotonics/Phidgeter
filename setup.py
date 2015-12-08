94434e097d4df51d3839f38b0293c4527c5338b5
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-12-08 10:43:09 -0500

    new style setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import os
 
-config = {
-    "description": "logging and convenience functions for phidgets",
-    "author": "Nathan Harrington",
-    "url": "https://github.com/nharringtonwasatch/Phidgeter",
-    "download_url": "https://github.com/nharringtonwasatch/Phidgeter",
-    "author_email": "nharrington@wasatchphotonics.com",
-    "version": "1.0.1",
-    #"install_requires": ["nose", "testfixtures", "phidgets"],
-    "packages": ["phidgeter"],
-    "scripts": [],
-    "name": "Phidgeter"
-}
+from setuptools import setup, find_packages
 
-setup(**config)
+# These will break the travis build.
+#here = os.path.abspath(os.path.dirname(__file__))
+#with open(os.path.join(here, "README.txt")) as f:
+#    README = f.read()
+#with open(os.path.join(here, "CHANGES.txt")) as f:
+#    CHANGES = f.read()
+
+README=""
+CHANGES=""
+
+requires = [
+    "pytest",
+    "pytest-cov",
+    "pytest-qt",
+    "phidgets",
+    ]
+
+setup(name="phidgeter",
+      version="0.0",
+      description="phidgeter - logging and control wrappers for Phidgets",
+      long_description=README + "\n\n" + CHANGES,
+      classifiers=[],
+      author="",
+      author_email="",
+      url="",
+      keywords="",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite="phidgeter",
+      install_requires=requires,
+      )
